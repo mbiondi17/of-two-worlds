025690fdec88466a96747d6ac6a876b7c80f3b6f
epicpython <jr3python@gmail.com>
2022-01-29 17:21:10 -0500

    Added space key under Player Actions Input
    Added space key under player actions
    
    plan is to use space to switch between light and dark

---
+++ Assets/Scripts/LightToDark.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToDark : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("space key was pressed");
+        } */
+        //does not work because we are using PlayerInput instead
+    }
+}
